9c04380e5be91c0b016be7c5b33b9877f15ddade
vidhya <vidhya.inboxme@gmail.com>
2021-11-12 19:23:08 +0530

    added video

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import logo from "./logo.svg";
-
 import React from "react";
+
+import VideoFile from "./video1.mp4";
 
 import "./App.css";
 
@@ -13,15 +13,12 @@ const App = () => {
   let initialY;
   let xOffset = 0;
   let yOffset = 0;
-  let right = 0;
-  let bottom = 0;
-  let newValues;
+  // let right = 0;
+  // let bottom = 0;
 
   React.useEffect(() => {
     const container = document.querySelector("#container");
-    const rect = container.getBoundingClientRect();
-    right = rect.right;
-    bottom = rect.bottom;
+    // const rect = container.getBoundingClientRect();
     container.addEventListener("touchstart", onDragStart, false);
     container.addEventListener("touchend", onDragEnd, false);
     container.addEventListener("touchmove", onDrag, false);
@@ -47,47 +44,54 @@ const App = () => {
 
   const onDragEnd = (e) => {
     active = false;
-    console.log("newValues x "+ newValues.x);
-    console.log("newValues y "+ newValues.y);
-    console.log("current x "+ currentX);
-    console.log("current y "+ currentY);
-    console.log("newValues.x - initialX "+ (newValues.x - initialX));
-    console.log("newValues.y - initialY"+ (newValues.y - initialY));
-    // const dragItem = document.querySelector("#imgId");
-    // setTranslate(currentX, currentY, dragItem);
     initialX = currentX;
     initialY = currentY;
   };
 
-  const getCornerCoord = (xVal, yVal) => {
-    let newX;
-    let newY;
-    if ((xVal >=0 && xVal <= right/2) && (yVal >=0 && yVal <= bottom/2)) {
-      //quarter1
-      console.log("quarter1");
-      newX = 0;
-      newY = 0;
-    } else if ((xVal >right/2 && xVal <= right) && (yVal >=0 && yVal <= bottom/2)) {
-      //quarter2
-      console.log("quarter2");
-      newX = right;
-      newY = 0;
-    } else if ((xVal >=0 && xVal <= right/2) && (yVal >bottom/2 && yVal <= bottom)) {
-      //quarter3
-      console.log("quarter3");
-      newX = 0;
-      newY = bottom;
-    } else if ((xVal >right/2 && xVal <= right) && (yVal >bottom/2 && yVal <= bottom)) {
-      //quarter4
-      console.log("quarter4");
-      newX = right;
-      newY = bottom;
-    }
-    return {
-      x: newX,
-      y: newY
-    }
-  }
+  // const getCornerCoord = (xVal, yVal) => {
+  //   let newX;
+  //   let newY;
+  //   if (xVal >= 0 && xVal <= right / 2 && yVal >= 0 && yVal <= bottom / 2) {
+  //     //quarter1
+  //     console.log("quarter1");
+  //     newX = 0;
+  //     newY = 0;
+  //   } else if (
+  //     xVal > right / 2 &&
+  //     xVal <= right &&
+  //     yVal >= 0 &&
+  //     yVal <= bottom / 2
+  //   ) {
+  //     //quarter2
+  //     console.log("quarter2");
+  //     newX = right;
+  //     newY = 0;
+  //   } else if (
+  //     xVal >= 0 &&
+  //     xVal <= right / 2 &&
+  //     yVal > bottom / 2 &&
+  //     yVal <= bottom
+  //   ) {
+  //     //quarter3
+  //     console.log("quarter3");
+  //     newX = 0;
+  //     newY = bottom;
+  //   } else if (
+  //     xVal > right / 2 &&
+  //     xVal <= right &&
+  //     yVal > bottom / 2 &&
+  //     yVal <= bottom
+  //   ) {
+  //     //quarter4
+  //     console.log("quarter4");
+  //     newX = right;
+  //     newY = bottom;
+  //   }
+  //   return {
+  //     x: newX,
+  //     y: newY,
+  //   };
+  // };
 
   const onDrag = (e) => {
     const dragItem = document.querySelector("#imgId");
@@ -104,7 +108,7 @@ const App = () => {
       }
       currentX = clientX - initialX;
       currentY = clientY - initialY;
-      newValues = getCornerCoord(clientX, clientY);
+      // newValues = getCornerCoord(clientX, clientY);
       xOffset = currentX;
       yOffset = currentY;
       setTranslate(currentX, currentY, dragItem);
@@ -116,8 +120,11 @@ const App = () => {
   };
 
   return (
-    <div id="container" className="container" >
-      <img src={logo} alt="" id="imgId" className="image" />
+    <div id="container" className="container">
+      <video controls width="200" height="300" id="imgId">
+        <source src={VideoFile} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
     </div>
   );
 };
